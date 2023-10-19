3c6fc2a1c1e616f8dcea24af05c434f911319557
rimgrs <61545065+rimgrs@users.noreply.github.com>
2023-10-19 12:26:23 +0300

    Update streamlit_app.py
    additional text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
