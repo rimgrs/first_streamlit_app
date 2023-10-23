043defa267116e8ae6c1b556299854df4c8f1394
rimgrs <61545065+rimgrs@users.noreply.github.com>
2023-10-23 21:18:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ streamlit.dataframe(fruits_to_show)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------- Lab 9/2
 #create the repetable code block (called a function)
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
