486bd280dc990f34c16f55996033c76102035906
rimgrs <61545065+rimgrs@users.noreply.github.com>
2023-10-23 21:01:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,16 +32,13 @@ streamlit.dataframe(fruits_to_show)
 #------------------------------------------------------------------------------------------------------------------------------------------------------- Lab 9/2
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-streamlit.text(fruityvice_response.json()) #just write the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-#take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
 
 #imports snoflace connector
 
