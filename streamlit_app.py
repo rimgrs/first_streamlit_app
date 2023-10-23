aa77a8d2e0126c1924deb3133e035bb26743e371
rimgrs <61545065+rimgrs@users.noreply.github.com>
2023-10-23 20:57:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -12,7 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
-import pandas
+#import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -31,17 +35,15 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 streamlit.text(fruityvice_response.json()) #just write the data to the screen
 
 #take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
-
+#imports snoflace connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -55,3 +57,16 @@ streamlit.dataframe(my_data_rows)
 
 #This will not work correctly ,but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+#don't run anything past here while troubleshoot
+streamlit.stop()
+
+import snowflake.connectot
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header ("The fruitload list contains")
+streamlit.dataframe(my_data_rows)
+
